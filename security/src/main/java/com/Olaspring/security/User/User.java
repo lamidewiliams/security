6ede1312797650e52d365fecf1b0c7a2cdfea8e0
lamidewiliams <lamidewiliams@gmail.com>
2024-04-24 13:35:10 +0100

    springsecurity

---
+++ security/src/main/java/com/Olaspring/security/User/User.java
@@ -0,0 +1,23 @@
+package com.Olaspring.security.User;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "jwtsecurity")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}1`
